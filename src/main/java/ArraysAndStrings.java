0997b5d4338207546a28d84c9529196a5b59f75a
yael <codingfreely@gmail.com>
2016-12-31 16:32:06 +0200

    implementations for determining if a string has all unique characters. q 1.1 page 90

---
+++ src/main/java/ArraysAndStrings.java
@@ -0,0 +1,91 @@
+import jdk.nashorn.internal.runtime.BitVector;
+
+import java.util.ArrayList;
+
+/**
+ * Created by yael on 31/12/16.
+ */
+public class ArraysAndStrings {
+
+    //Question 1.1 page 90
+    //Determines if a string has all unique characters with and without using additional data structure.
+
+    /**
+     * Running time O(n^2). no additional space used.
+     * @param s
+     * @return
+     */
+    public static boolean isUnique(String s){
+        for (int i = 0; i < s.length(); i++) {
+            char curChar = s.charAt(i);
+            for (int j = 0; j < i; j++) {
+                if(s.charAt(j) == curChar) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * running time O(n). constant space. assuming characters a-z for the lengths.
+     * should be larger if to support other characters.
+     * @param s
+     * @return
+     */
+    public static boolean isUniqueUsingBitVector(String s){
+        BitVector bv = new BitVector(26);
+        for (int i = 0; i < s.length(); i++) {
+            int charInx = getCharInx(s.charAt(i));
+            if(bv.isSet(charInx)) {
+                return false;
+            } else {
+                bv.set(charInx);
+            }
+        }
+        return true;
+    }
+
+    /**
+     * running time O(NlogN). O(N) space.
+     * Assuming sort is O(NlogN) and is not using additional space
+     * @param s
+     * @return
+     */
+    public static boolean isUniqueUsingSort(String s){
+        ArrayList<Character> characters = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            characters.add(s.charAt(i));
+        }
+
+        characters.sort(Character::compareTo);
+
+        int last = -1;
+        for (int i = 0; i < characters.size(); i++) {
+            int cur = getCharInx(characters.get(i));
+            if( cur == last){
+                return false;
+            } else {
+                last = cur;
+            }
+        }
+        return true;
+    }
+
+    private static int getCharInx(char c){
+        return c - 'a';
+    }
+
+    public static boolean isUniqueBookSolution(String s){
+        int checker = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int val = getCharInx(s.charAt(i));
+            if((checker & (1 << val)) > 0){
+                return false;
+            }
+            checker |= (1 << val);
+        }
+        return true;
+    }
+
+}
---
+++ src/test/java/ArraysAndStringsTest.java
@@ -0,0 +1,33 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by yael on 31/12/16.
+ */
+public class ArraysAndStringsTest {
+
+    @Test
+    public void testIsUnique(){
+        Assert.assertTrue(ArraysAndStrings.isUnique("abcdefghijklmnop"));
+        Assert.assertFalse(ArraysAndStrings.isUnique("abcdefghijklmnopa"));
+    }
+
+    @Test
+    public void testIsUniqueWithBitVector(){
+        Assert.assertTrue(ArraysAndStrings.isUniqueUsingBitVector("abcdefghijklmnopz"));
+        Assert.assertFalse(ArraysAndStrings.isUniqueUsingBitVector("abcdefghijdklmsnopz"));
+    }
+
+    @Test
+    public void testIsUniqueUsingSort(){
+        Assert.assertTrue(ArraysAndStrings.isUniqueUsingSort("abcdefghijklmnopz"));
+        Assert.assertFalse(ArraysAndStrings.isUniqueUsingSort("abcdefghijklmnopaz"));
+    }
+
+    @Test
+    public void testIsUniqueBookSol(){
+        Assert.assertTrue(ArraysAndStrings.isUniqueBookSolution("abcdefghijklmnopz"));
+        Assert.assertFalse(ArraysAndStrings.isUniqueBookSolution("abcdefghijklmnopaz"));
+    }
+
+}
