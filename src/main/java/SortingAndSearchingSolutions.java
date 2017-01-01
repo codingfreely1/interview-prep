f879ae7cf659c1d88acb78c82c3df8002ac9734c
yael <codingfreely@gmail.com>
2017-01-01 22:17:30 +0200

    no need to iterate over all of a just until b's index is in range. Added implementation using primitive array

--- src/main/java/SortingAndSearchingSolutions.java
+++ src/main/java/SortingAndSearchingSolutions.java
@@ -11,27 +11,37 @@ public class SortingAndSearchingSolutions {
      * @param b
      * @param <T>
      */
-    public static <T extends Comparable<T>> void mergeBIntoA (List<T> a, List<T> b) {
+    public static <T extends Comparable<T>> void mergeBIntoA(List<T> a, List<T> b) {
         int i = b.size() -1;
         int j = a.size() - b.size() -1;
         int k = a.size() -1;
-        while(k >= 0) {
-            if(i >= 0 && j >= 0){
-                if(a.get(j).compareTo(b.get(i)) > 0){
-                    a.set(k,a.get(j));
-                    j--;
-                } else {
-                    a.set(k,b.get(i));
-                    i--;
-                }
-            } else if (i>=0) {
-                a.set(k,b.get(i));
-                i--;
-            } else if(j>=0){
-                a.set(k,a.get(j));
+        while(i >= 0) {
+            if( a.get(j).compareTo(b.get(i)) > 0 ){
+                a.set(k, a.get(j));
                 j--;
+            } else {
+                a.set(k, b.get(i));
+                i--;
             }
             k--;
+        }
+
+    }
+
+    public static void mergeBIntoA(int[] a, int[] b){
+        int bInx = b.length - 1;
+        int aInx = a.length - b.length -1;
+        int aCurInx = a.length -1;
+
+        while(bInx >= 0){
+            if(a[aInx] > b[bInx]) {
+                a[aCurInx] = a[aInx];
+                aInx--;
+            } else {
+                a[aCurInx] = b[bInx];
+                bInx--;
+            }
+            aCurInx--;
         }
     }
 }
--- src/test/java/SortedAndSearchingSolutionsTest.java
+++ src/test/java/SortedAndSearchingSolutionsTest.java
@@ -19,4 +19,16 @@ public class SortedAndSearchingSolutionsTest {
         Assert.assertEquals(Arrays.asList(0,1,2,5,6,7,9,10), a);
     }
 
+    @Test
+    public void testSortedMerge2(){
+        int[] a = {0,2,6,7,9,-1,-1,-1};
+        int[] b = {1,5,10};
+        int[] expected = {0,1,2,5,6,7,9,10};
+
+        SortingAndSearchingSolutions.mergeBIntoA(a,b);
+        for (int i = 0; i < expected.length; i++) {
+            Assert.assertEquals(expected[i], a[i]);
+        }
+    }
+
 }
